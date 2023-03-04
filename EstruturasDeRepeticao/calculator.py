626fde5c42b420988268b3d893093cad58203022
Klayvert <klayvert.ryan16@gmail.com>
2023-03-04 19:07:48 -0300

    Finishing Calculator Project

--- EstruturasDeRepeticao/calculator.py
+++ EstruturasDeRepeticao/calculator.py
@@ -1,24 +1,19 @@
-while True:
-    sair = input('Deseja sair? [S/N] ')
-    if sair.lower() == 's':
-        break
-    opcao = input('Digite qual operação deseja fazer: (soma, subtracao, multiplicacao ou divisao): ')
-    if opcao.lower() != 'soma' and 'subtracao' and 'multiplicacao' and 'divisao':
-        print('Digite uma opção válida!!!')
-        continue
-    
-    num1 = float(input('Digite o primeiro número da operação: \n'))
-    num2 = float(input('Digite o segundo número da operação: \n'))
+from operations import Operations
 
-    if opcao == 'soma':
-        total = num1 + num2
-        print(total)
-    elif opcao == 'subtracao':
-        total = num1 - num2
-        print(total)
-    elif opcao == 'multiplicacao':
-        total = num1 * num2
-        print(total)
-    else:
-        total = num1 / num2
-        print(total)
+while True:
+    opcao = input('Digite qual operação deseja fazer: ([1]Soma, [2]Subtração, [3]Multiplicação ou [4]Divisão): ').lower()
+    operacoes = [1, 2, 3, 4, 'soma', 'subtracao', 'subtração', 'multiplicacao', 'multiplicação', 'divisão', 'divisao']
+    if opcao not in operacoes:
+        print('Digite uma operação válida!!!')
+        continue
+    try:
+        num1 = float(input('Digite o primeiro número da operação: \n'))
+        num2 = float(input('Digite o segundo número da operação: \n'))
+        Operations(opcao=opcao, num1=num1, num2=num2)
+    except ValueError:
+        print('Digite apenas números!!!')
+        continue
+
+    sair = input('Deseja sair do programa? [S]im ou [N]ão ').lower().startswith('s')
+    if sair:
+        break
