3e57f5664e75a12121bee7de83a178c9aa62bd8f
Klayvert <klayvert.ryan16@gmail.com>
2023-03-04 11:21:53 -0300

    Adding example of a while inside another

--- EstruturasDeRepeticao/while.py
+++ EstruturasDeRepeticao/while.py
@@ -29,11 +29,49 @@ while resposta != 'sim' or 'SIM' or 'Sim' or 'S' or 's':
 
 #Exemplo 3:
 
-condicao = True
+# condicao = True
 
-while condicao:
-    nome = input('Digite seu nome: ')
-    print(f'Seu nome é {nome}')
-    if nome == 'Klayvert':
-        condicao = False
-        #break
+# while condicao:
+#     nome = input('Digite seu nome: ')
+#     print(f'Seu nome é {nome}')
+#     if nome == 'Klayvert':
+#         condicao = False
+#         break
+
+# contador = 0
+
+# while contador < 100:
+#     contador += 1
+
+#     # Não executa os blocos de código abaixo do if e volta no loop novamente se contador == 6
+#     if contador == 6:
+#         continue
+
+#     # Pulando laço do while do número 10 ao número 20
+#     if contador >= 10 and contador <= 20: 
+#         continue
+
+#     print(f'O laço repetiu {contador} vez(es)')
+
+#     # Quebrando o laço While se o contador chegar ao 30
+#     if contador == 30:
+#         print(contador)
+#         break
+
+qtd_linhas = 5
+qtd_colunas = 5
+
+linha = 1
+
+# Cria laço while normal para adicionar linhas
+while linha <= qtd_linhas: 
+    coluna = 1
+    #Cria laço normal do while dentro de outro while que irá executar até que  
+    #a expressão seja falsa e somente após isso voltará para o primeiro while(o que ele está dentro)
+    while coluna <= qtd_colunas:
+        print(f'{linha=}, {coluna=}')
+        coluna += 1 
+    linha += 1
+    
+
+print("Acabou")
